fad55963cbaf49264f8cb1f3bf51d64939c32ee4
eniolakura <87645727+eniolakura@users.noreply.github.com>
2022-06-22 20:24:15 +0100

    Rename main.js to app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+let Name = "Eniola Kuranga"
+let height = "195cm"
+let country = "Nigeria"
+alert("Hi,my name is "+ Name+". i am "+ height+" tall and i am from "+ country );
--- main.js
+++
@@ -1,4 +0,0 @@
-let Name = "Eniola Kuranga"
-let height = "195cm"
-let country = "Nigeria"
-alert("Hi,my name is "+ Name+". i am "+ height+" tall and i am from "+ country );
